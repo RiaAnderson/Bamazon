8b92def24ee5c226b292869e4a647c9ac9fbf733
Ria <astoriaa92@gmail.com>
2019-10-15 20:28:44 -0700

    Added function for user to order products from the table.

--- bamazon.js
+++ bamazon.js
@@ -10,5 +10,25 @@ var connection = mysql.createConnection({
 
 
 //Select table to order products from
-//User will input item_id and quantity
+connection.query('SELECT * FROM products', function (error, results){
+    if (error) throw error;
+    //display result in table format 
+    console.table(results);
+    //Ask user for item_id and quantity
+    inquirer.prompt(
+        [
+            {
+                type: "input",
+                message: "What is the item ID of the product would you like to purchase?",
+                name: "productID"
+            },
+            {
+                type: "input",
+                message: "How many would you like to purchase?",
+                name: "itemQuantity"
+            },
+        ]
+    )
+})
+
 //Response to user request
