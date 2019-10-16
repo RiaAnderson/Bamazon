5c5e740e2e34c7f8410a3b6ea5a32ff8f722e12a
Ria <astoriaa92@gmail.com>
2019-10-15 20:20:54 -0700

    Added pseudocode to js.

--- bamazon.js
+++ bamazon.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'bamazonDB'
 });
+
+
+//Select table to order products from
+//User will input item_id and quantity
+//Response to user request
